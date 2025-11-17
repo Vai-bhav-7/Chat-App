4321ca74f2b9ae6853840ff168577bb218dce939
Vaibhav <vaibhavtrivedi02468@gmail.com>
2025-11-17 15:53:41 +0530

    Import mongoose and fix MongoDB connection

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
 import { Server } from "socket.io";
+import mongoose from "mongoose";   
 
 const app = express();
 const server = http.createServer(app)
